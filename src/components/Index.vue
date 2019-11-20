872c06b81f686f349db46f335b09b5499ce18b90
thanthtetzin <thanthtetzin1990@gmail.com>
2019-11-20 23:58:34 +0700

    Seperating components

---
+++ src/components/Index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>    
+    <div id="ad">
+      ADVERTISEMENT
+    </div>
+    <PageVisibility 
+      v-bind:ads="ads"
+    />
+  </div>
+ 
+</template>
+
+<script>
+import PageVisibility from './PageVisibility.vue';
+
+export default {
+  name: 'Index',
+  components: {
+    PageVisibility
+  },
+  data() {
+    return {
+      ads:[
+        {elementId: "ad", elementName: "ADVERTISEMENT"},
+        {elementId: "ad2", elementName: "ADVERTISEMENT2"}
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+body {
+  height: 2000px;
+  width: 2500px;
+}
+#ad {
+  width: 300px;
+  height: 250px;
+  background: #ee7202;
+  position: relative;
+  left: 100px;
+  top: 100px;
+}
+</style>
---
+++ src/components/PageVisibility.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-card class="page-visibility-card" title="Card title" sub-title="Card subtitle" no-body>
+      <b-table 
+        striped 
+        hover 
+        :items="tableItems"
+        thead-class="hidden_header"
+      ></b-table>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageVisibility',
+  props: {
+    msg: String,
+    ads: Array
+  },
+  data(){
+    return{
+      advertisements: this.ads,
+      tableItems: []
+    }
+  },
+  mounted(){
+    this.advertisements.forEach((advertisement) => {
+      this.tableItems.push({
+        elementId: advertisement.elementId,
+        elementName: advertisement.elementName,
+        viewdPeriod: "0s",
+        viewablePercentage: "0%",
+        greaterThanEqual50Percent: false
+      })
+    });
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.page-visibility-card{
+  position: fixed !important;
+  right: 25%;
+  width: 40%;
+  top: 23%;
+  box-shadow: 3px 3px 5px 0px rgba(0, 0, 0, 0.5);
+}
+</style>
